038afbee37906e5dfad70d14885187c2a574cdcb
amitvolga <amit.shimon@gmail.com>
2017-06-13 19:11:17 +0300

    Update CompanyDBDAO.java

--- src/dbDao/CompanyDBDAO.java
+++ src/dbDao/CompanyDBDAO.java
@@ -226,19 +226,7 @@ public class CompanyDBDAO implements CompenyDAO {
 				c.setMassage(results.getString("massage"));
 				coupons.add(c);
 			}
-			if (coupons.size() <= 1) {
-				Coupon c = new Coupon();
-				c.setAmount(0);
-				c.setEndDate("2088-09-09");
-				c.setStartDate("2017-09-01");
-				c.setMassage("Buy this Coupon space");
-				c.setId(0);
-				c.setTitle("Buy this Coupon space");
-				c.setPrice(0);
-				c.setImag("Buy this Coupon space");
-				c.setType(CouponType.CAMPING);
-				coupons.add(c);
-			}
+			
 			company.setCoupons(coupons);
 
 			return coupons;
